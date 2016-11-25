12aff84daea607495b7c2b50e1cab9966ec4717f
Shlomi Fish <shlomif@shlomifish.org>
2016-11-25 15:57:56 +0200

    Add flags.

--- reproduce.sh
+++ reproduce.sh
@@ -5,4 +5,4 @@ in_fn='t.txt'
 if ! test -e "$in_fn" ; then
     seq 1 1000000 > "$in_fn"
 fi
-gvim "$in_fn" +500000 +"normal zz" +"normal VG"
+gvim -u NONE -U NONE "$in_fn" +500000 +"normal zz" +"normal VG"
